321e311318e06a58a5a1af9bf0ec3e68d0e79040
silasviniciuss <codando1@gmail.com>
2022-03-17 11:33:21 -0300

    criando conexão com o banco

---
+++ src/projeto/dao/ClientesDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.dao;
+
+/**
+ *
+ * @author Silas Vinicius
+ */
+public class ClientesDAO {
+    
+    //metodo cadastrar cliente
+    public void cadastrarCliente(){
+    
+        
+        
+    };
+    
+    
+    //metodo alterar cliente
+    public void alterarCliente(){
+    
+        
+        
+    };
+    
+    //metodo excluir cliente
+    public void excluirCliente(){
+    
+        
+        
+    };
+    
+};
